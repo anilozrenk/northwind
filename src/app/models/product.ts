48e76f5eeebb054d0f608aabecc6da84773afe65
Anıl Özrenk <anil.ozrenk@gmail.com>
2021-03-11 17:47:24 +0300

    :)

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+import { SystemJsNgModuleLoaderConfig } from "@angular/core";
+
+export interface Product{
+productId:number;
+categoryId:number;
+productName:string;
+unitsInStock:number;
+unitPrice:number;
+}
