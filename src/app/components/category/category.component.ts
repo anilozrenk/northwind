f2ccba7252a2d7e27e43e7e6b659a39e090b2726
Anıl Özrenk <anil.ozrenk@gmail.com>
2021-03-14 00:38:14 +0300

    .

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -8,25 +8,19 @@ import { CategoryService } from 'src/app/services/category.service';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
   categories: Category[] = [];
   currentCategory: Category;
-
   constructor(private categoryService: CategoryService) { }
-
   ngOnInit(): void {
     this.getCategories();
   }
-
   getCategories() {
     this.categoryService.getCategories().subscribe(response => {
       this.categories = response.data
     })
-
   }
   setCurrentCategory(category: Category) {
     this.currentCategory = category;
-
   }
   getCurrentCategoryClass(category: Category) {
     if (category == this.currentCategory) {
@@ -42,4 +36,5 @@ export class CategoryComponent implements OnInit {
       return "list-group-item"
     }
   }
+
 }
